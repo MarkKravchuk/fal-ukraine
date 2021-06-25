be99b876c100b56fe6c01dc8a0019941233baf7b
Andrii Roizin <465182@student.saxion.nl>
2021-06-25 15:34:52 +0200

    crew date picker

--- src/components/crewFormComponent.js
+++ src/components/crewFormComponent.js
@@ -3,6 +3,10 @@ import ReactDataGrid from 'react-data-grid';
 import {withStyles} from '@material-ui/core/styles';
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import { Editors, Formatters } from 'react-data-grid-addons';
+import dataGridDatePicker from "./dataGridDatePicker";
+const { DropDownEditor } = Editors;
+const { DropDownFormatter } = Formatters;
 
 const styles = (theme) => ({
     root: {
@@ -13,17 +17,26 @@ const styles = (theme) => ({
     },
 });
 
+const genders = ["(...)",'Male','Female']
+const GendersEditor = <DropDownEditor options={genders}  />;
+
+const idTypes = ["(...)",'Musterbook','Passport','Residential permit', 'Picture id','Others']
+const IDTypesEditor = <DropDownEditor options={idTypes}  />;
+
+const RanksOfRating = ["(...)", 'AbleSeaman', 'Agent', 'AsstFoodBevMngr', 'BarManager', 'BarService', 'Bosun', 'Cadet', 'Captain', 'CargoTechnician', 'CasinoStaff', 'ChiefCook', 'ChiefElectrician', 'ChiefHousekeeper', 'ChiefEngineer', 'ChiefMaster', 'ChiefMate', 'ChiefOfficer', 'ChiefPurser', 'ChiefSteward', 'ClassSurveyor', 'CSO', 'Cook', 'CraneOperator', 'CrewMember', 'CruiseDirector', 'CruiseStaff', 'DeckApprentice', 'DeckFitter', 'DeckOfficer', 'Deckhand', 'Doctor', 'Donkeyman', 'ElectricalEngineer', 'ElectricalOfficer', 'Electrician', 'EngineerCadet', 'EngineeringCrew', 'EngineFitter', 'Entertainment', 'FacilitiesCrew', 'FacilitiesManager', 'FirstAsstEngineer', 'FirstEngineer', 'FirstMate', 'FirstOfficer', 'Fitter', 'FourthOfficer', 'FoodBevMngr', 'FoodService', 'FourthAsstEngineer', 'Greaser', 'Hospitality', 'HotelDirector', 'HotelStaff', 'HousekeepingStaff', 'InformationTechnology', 'JuniorEngineer', 'LaundryMaster', 'Lifeboatman', 'Maitred', 'MarineCrew', 'MarketingRevenueMngr', 'Master', 'MasterFirstClassPilot', 'MateFirstClassPilot', 'Mechanic', 'MedicalStaff', 'Messman', 'Motorman', 'Oiler', 'Operator', 'OrdinarySeaman', 'Owner', 'Painter', 'Porter', 'Provision', 'ProvisionMaster', 'Pumpman', 'QMED', 'RadioOfficer', 'Reeferman', 'Repairman', 'RiddingCrew', 'SafetyAndSecurity', 'SecondAsstEngineer', 'SecondMate', 'SecondOfficer', 'SSO', 'StaffCaptain', 'Steward', 'Superintendent', 'Tankerman', 'ThirdAsstEngineer', 'ThirdMate', 'ThirdOfficer', 'ThirdParty', 'TruckMechanic', 'Tunnelman', 'UtilityPerson', 'VettingInspector', 'Welder', 'Wiper', 'YardPersonnel', 'Other']
+const RanksOfRatingEditor = <DropDownEditor options={RanksOfRating}  />;
+
 const columns = [
     { key: "NR", name: "NR", editable: true, width: 50},
     { key: "Family name", name: "Family name", editable: true },
     { key: "Given name", name: "Given name", editable: true },
-    { key: "Gender", name: "Gender", editable: true, width: 80 },
-    { key: "Rank of rating", name: "Rank of rating", editable: true },
+    { key: "Gender", name: "Gender", editable: true, width: 80, editor: GendersEditor },
+    { key: "Rank of rating", name: "Rank of rating", editable: true, editor:RanksOfRatingEditor },
     { key: "Nationality", name: "Nationality", editable: true },
     { key: "Country of birth", name: "Country of birth", editable: true },
     { key: "Place of birth", name: "Place of birth", editable: true },
-    { key: "Date of birth", name: "Date of birth", editable: true },
-    { key: "ID type", name: "ID type", editable: true, width: 80},
+    { key: "date_of_birth", name: "Date of birth", editable: true, editor:dataGridDatePicker },
+    { key: "ID type", name: "ID type", editable: true, width: 80, editor: IDTypesEditor},
     { key: "ID document number", name: "ID document number", editable: true },
     { key: "Issuing state of identity document", name: "Issuing state of identity document", editable: true },
     { key: "Expiry date of identity document", name: "Expiry date of identity document", editable: true },
@@ -31,12 +44,13 @@ const columns = [
 ];
 
 const rows = [
-    { NR: 1 },
+    { NR: 1, },
     { NR: 2 },
     { NR: 3 },
     { NR: 4 },
     { NR: 5 },
 ];
+
 
 
 
@@ -54,15 +68,16 @@ class CrewForm extends React.Component{
 
 
     onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
-        // this.setState(state => {
-        //     const rows = state.rows.slice();
-        //     for (let i = fromRow; i <= toRow; i++) {
-        //         rows[i] = { ...rows[i], ...updated };
-        //     }
-        //     return { rows };
-        // });
+        this.setState(state => {
+            const rows = state.rows.slice();
+            for (let i = fromRow; i <= toRow; i++) {
+                rows[i] = { ...rows[i], ...updated };
+            }
+            return { rows };
+        });
     };
     render() {
+        console.log("rows ", this.state.rows)
         return (
             <div>    <Typography variant="h3" component="h3" gutterBottom>
                 Crew list
@@ -81,16 +96,7 @@ class CrewForm extends React.Component{
     }
 
 
-    // const [gridState, setGridState] = React.useState(rows);
-    //
-    // return (
-    //     <ReactDataGrid
-    //         columns={columns}
-    //         rows={gridState}
-    //         rowGetter={i => gridState[i]}
-    //         rowsCount={3}
-    //     />
-    // );
+
 
 
 
