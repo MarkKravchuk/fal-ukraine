735af6fdd378dfa6cf688a1d86519436b90bf1d1
Andrii Roizin <465182@student.saxion.nl>
2021-07-12 12:23:47 +0200

    generate xml port without purposes of call

--- src/functions/generateXML.js
+++ src/functions/generateXML.js
@@ -1,11 +1,34 @@
 import xml from 'xml'
+import {forEach} from "read-excel-file/modules/xml/dom";
+import {element} from "prop-types";
+import listOfPortsConst from "../config/consts/listOfPortsConst";
+
 
 const RanksOfRating = ['AbleSeaman', 'Agent', 'AsstFoodBevMngr', 'BarManager', 'BarService', 'Bosun', 'Cadet', 'Captain', 'CargoTechnician', 'CasinoStaff', 'ChiefCook', 'ChiefElectrician', 'ChiefHousekeeper', 'ChiefEngineer', 'ChiefMaster', 'ChiefMate', 'ChiefOfficer', 'ChiefPurser', 'ChiefSteward', 'ClassSurveyor', 'CSO', 'Cook', 'CraneOperator', 'CrewMember', 'CruiseDirector', 'CruiseStaff', 'DeckApprentice', 'DeckFitter', 'DeckOfficer', 'Deckhand', 'Doctor', 'Donkeyman', 'ElectricalEngineer', 'ElectricalOfficer', 'Electrician', 'EngineerCadet', 'EngineeringCrew', 'EngineFitter', 'Entertainment', 'FacilitiesCrew', 'FacilitiesManager', 'FirstAsstEngineer', 'FirstEngineer', 'FirstMate', 'FirstOfficer', 'Fitter', 'FourthOfficer', 'FoodBevMngr', 'FoodService', 'FourthAsstEngineer', 'Greaser', 'Hospitality', 'HotelDirector', 'HotelStaff', 'HousekeepingStaff', 'InformationTechnology', 'JuniorEngineer', 'LaundryMaster', 'Lifeboatman', 'Maitred', 'MarineCrew', 'MarketingRevenueMngr', 'Master', 'MasterFirstClassPilot', 'MateFirstClassPilot', 'Mechanic', 'MedicalStaff', 'Messman', 'Motorman', 'Oiler', 'Operator', 'OrdinarySeaman', 'Owner', 'Painter', 'Porter', 'Provision', 'ProvisionMaster', 'Pumpman', 'QMED', 'RadioOfficer', 'Reeferman', 'Repairman', 'RiddingCrew', 'SafetyAndSecurity', 'SecondAsstEngineer', 'SecondMate', 'SecondOfficer', 'SSO', 'StaffCaptain', 'Steward', 'Superintendent', 'Tankerman', 'ThirdAsstEngineer', 'ThirdMate', 'ThirdOfficer', 'ThirdParty', 'TruckMechanic', 'Tunnelman', 'UtilityPerson', 'VettingInspector', 'Welder', 'Wiper', 'YardPersonnel', 'Other']
 
 function createXML(data) {
+
     if (dataCheck(data) === true) {
         let port = data.port;
         let crew = data.crew.rows;
+        let portOfCall = listOfPortsConst.find(function(element) {
+            return element.code === port.portOfCall;
+        });
+
+        let portOfArrival = listOfPortsConst.find(function(element) {
+            return element.code === port.portOfArrival;
+        });
+
+        let lastPortOfCall = listOfPortsConst.find(function(element) {
+            return element.code === port.lastPortOfCall;
+        });
+        let nextPortOfCall = listOfPortsConst.find(function(element) {
+            return element.code === port.nextPortOfCall;
+        });
+        let callPurposes = [];
+        for (let i = 0; i <port.callPurpose.length; i++) {
+
+        }
 
         console.log("generatexmldata ", data);
         let ArrivalDepartureDraught;
@@ -63,6 +86,9 @@ function createXML(data) {
                 ]
             }
         }
+
+
+        console.log("POC ",port.portOfCall)
         let xmlValue = xml([{
             EPCMessage: [{
                 EPCMessageHeader: [
@@ -78,10 +104,10 @@ function createXML(data) {
                                     PortCall: [
                                         {
                                             Port: [
-                                                {Name: port.portOfCall.name},
-                                                {Facility: port.portFacilityAtArrival.Facility},
-                                                {CountryCode: port.portOfCall.CountryCode},
-                                                {UNLoCode: port.portOfCall.UNLoCode}
+                                                {Name: portOfCall.name},
+                                                {Facility: port.portFacilityAtArrival},
+                                                {CountryCode: portOfCall.countryCode},
+                                                {UNLoCode: portOfCall.code}
                                             ]
                                         }]
                                 }
@@ -94,25 +120,26 @@ function createXML(data) {
                         {ATDPortOfCall: port.ATDPortOfCall},
                         {
                             PortOfArrival: [
-                                {Name: port.portOfArrival.name},
-                                {CountryCode: port.portOfArrival.CountryCode},
-                                {UNLoCode: port.portOfArrival.UNLoCode},
+                                {Name: portOfArrival.name},
+                                {CountryCode: portOfArrival.countryCode},
+                                {UNLoCode: portOfArrival.code},
                             ]
                         },
                         {
                             LastPortOfCall: [
-                                {Name: port.lastPortOfCall.name},
-                                {CountryCode: port.lastPortOfCall.CountryCode},
-                                {UNLoCode: port.lastPortOfCall.UNLoCode}
+                                {Name: lastPortOfCall.name},
+                                {CountryCode: lastPortOfCall.countryCode},
+                                {UNLoCode: lastPortOfCall.code}
                             ]
                         },
                         {
                             NextPortOfCall: [
-                                {Name: port.nextPortOfCall.name},
-                                {CountryCode: port.nextPortOfCall.CountryCode},
-                                {UNLoCode: port.nextPortOfCall.UNLoCode}
+                                {Name: nextPortOfCall.name},
+                                {CountryCode: nextPortOfCall.countryCode},
+                                {UNLoCode: nextPortOfCall.code}
                             ]
                         },
+
                         {Anchorage: port.callAnchorage},
                         {
                             EntryPosition: [
@@ -127,88 +154,88 @@ function createXML(data) {
                         {CargoOverview: port.cargoDescription},
                         {
                             NameOfMaster: [
-                                {GivenName: port.nameMaster.givenName},
-                                {FamilyName: port.nameMaster.familyName},
+                                {GivenName: port.nameOfMaster.givenName},
+                                {FamilyName: port.nameOfMaster.familyName},
 
                             ]
                         },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[0].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[0].CallPurposeText}
-                            ]
-                        },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[1].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[1].CallPurposeText}
-                            ]
-                        },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[2].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[2].CallPurposeText}
-                            ]
-                        },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[3].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[3].CallPurposeText}
-                            ]
-                        },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[4].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[4].CallPurposeText}
-                            ]
-                        },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[5].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[5].CallPurposeText}
-                            ]
-                        },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[6].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[6].CallPurposeText}
-                            ]
-                        },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[7].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[7].CallPurposeText}
-                            ]
-                        },
-                        {
-                            CallPurpose: [
-                                {CallPurposeCode: port.purposesOfCall[8].CallPurposeCode},
-                                {CallPurposeText: port.purposesOfCall[8].CallPurposeText}
-                            ]
-                        },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[0].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[0].CallPurposeText}
+                        //     ]
+                        // },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[1].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[1].CallPurposeText}
+                        //     ]
+                        // },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[2].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[2].CallPurposeText}
+                        //     ]
+                        // },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[3].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[3].CallPurposeText}
+                        //     ]
+                        // },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[4].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[4].CallPurposeText}
+                        //     ]
+                        // },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[5].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[5].CallPurposeText}
+                        //     ]
+                        // },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[6].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[6].CallPurposeText}
+                        //     ]
+                        // },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[7].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[7].CallPurposeText}
+                        //     ]
+                        // },
+                        // {
+                        //     CallPurpose: [
+                        //         {CallPurposeCode: port.purposesOfCall[8].CallPurposeCode},
+                        //         {CallPurposeText: port.purposesOfCall[8].CallPurposeText}
+                        //     ]
+                        // },
                         {AirDraught: port.airDraught},
                         ArrivalDepartureDraught,
                         {
                             Agent: [
-                                {Company: port.agent.name},
+                                {Company: port.agent.company},
                                 {
                                     ContactNumbers: [
-                                        {BusinessTelephone: port.agent.businessTelephone},
-                                        {MobileTelephone: port.agent.mobileTelephone},
-                                        {Telefax: port.agent.telefax},
-                                        {Email: port.agent.email},
+                                        {BusinessTelephone: port.agent.contactNumbers.businessTelephone},
+                                        {MobileTelephone: port.agent.contactNumbers.mobileTelephone},
+                                        {Telefax: port.agent.contactNumbers.telefax},
+                                        {Email: port.agent.contactNumbers.EMail},
                                     ]
                                 },
                             ]
                         },
                         {
                             PersonsOnBoard: [
-                                {NumberOfPersonsOnBoard: port.personsOnBoard.numberOfPersons},
-                                {Passengers: port.personsOnBoard.numberOfPassengers},
-                                {Crew: port.personsOnBoard.numberOfCrew},
+                                {NumberOfPersonsOnBoard: port.personsOnBoard.numberOfPersonsOnBoard},
+                                {Passengers: port.personsOnBoard.passengers},
+                                {Crew: port.personsOnBoard.crew},
                             ]
                         },
-                        {Stowaways: port.Stowaways},
+                        {Stowaways: port.stowaways},
                         {PeriodOfStay: port.periodOfStay},
                         {CrewList: CrewList}
 
@@ -240,45 +267,45 @@ let downloadXMLfile = (xmlValue) => {
 }
 
 let dataCheck = (data) => {
-    for (let i = 0; i < data.crew.rows.length; i++) {
-        let row = data.crew.rows[i]
-        if (row.Family_name == null) {
-            alert("Please, fill in the required field 'Family name' in Crew list")
-            return false
-        }
-        if (row.Given_name == null) {
-            alert("Please, fill in the required field 'Given name' in Crew list")
-            return false
-        }
-        if (row.Rank_of_rating == null) {
-            alert("Please, fill in the required field 'Rank of rating' in Crew list")
-            return false
-        }
-        if (row.Nationality == null) {
-            alert("Please, fill in the required field 'Nationality' in Crew list")
-            return false
-        }
-        if (row.Country_of_birth == null) {
-            alert("Please, fill in the required field 'Country of birth' in Crew list")
-            return false
-        }
-        if (row.Place_of_birth == null) {
-            alert("Please, fill in the required field 'Place of birth' in Crew list")
-            return false
-        }
-        if (row.date_of_birth == null) {
-            alert("Please, fill in the required field 'date of birth' in Crew list")
-            return false
-        }
-        if (row.ID_type == null) {
-            alert("Please, fill in the required field 'ID type' in Crew list")
-            return false
-        }
-        if (row.ID_document_number == null) {
-            alert("Please, fill in the required field 'ID document number' in Crew list")
-            return false
-        }
-    }
+    // for (let i = 0; i < data.crew.rows.length; i++) {
+    //     let row = data.crew.rows[i]
+    //     if (row.Family_name == null) {
+    //         alert("Please, fill in the required field 'Family name' in Crew list")
+    //         return false
+    //     }
+    //     if (row.Given_name == null) {
+    //         alert("Please, fill in the required field 'Given name' in Crew list")
+    //         return false
+    //     }
+    //     if (row.Rank_of_rating == null) {
+    //         alert("Please, fill in the required field 'Rank of rating' in Crew list")
+    //         return false
+    //     }
+    //     if (row.Nationality == null) {
+    //         alert("Please, fill in the required field 'Nationality' in Crew list")
+    //         return false
+    //     }
+    //     if (row.Country_of_birth == null) {
+    //         alert("Please, fill in the required field 'Country of birth' in Crew list")
+    //         return false
+    //     }
+    //     if (row.Place_of_birth == null) {
+    //         alert("Please, fill in the required field 'Place of birth' in Crew list")
+    //         return false
+    //     }
+    //     if (row.date_of_birth == null) {
+    //         alert("Please, fill in the required field 'date of birth' in Crew list")
+    //         return false
+    //     }
+    //     if (row.ID_type == null) {
+    //         alert("Please, fill in the required field 'ID type' in Crew list")
+    //         return false
+    //     }
+    //     if (row.ID_document_number == null) {
+    //         alert("Please, fill in the required field 'ID document number' in Crew list")
+    //         return false
+    //     }
+    // }
     return true
 }
 
