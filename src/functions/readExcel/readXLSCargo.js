98602345e21c11b4297bd641e969a343d25a00c4
Andrii Roizin <465182@student.saxion.nl>
2021-07-23 23:01:42 +0200

    read xls cargo with dpg

--- src/functions/readExcel/readXLSCargo.js
+++ src/functions/readExcel/readXLSCargo.js
@@ -5,7 +5,9 @@ function readXLSCargo(file, onSave) {
     data.cargo.rows = [];
     data.cargo.portOfLoading = '';
     data.cargo.portOfDischarge = '';
+    data.dpg.rows = [];
     let cargo = data.cargo;
+    let dpg = data.dpg;
     readXlsxFile(file).then((rows) => {
         console.log("cargo ", rows);
         console.log("portOfLoading ", rows[72][3])
@@ -32,6 +34,26 @@ function readXLSCargo(file, onSave) {
             }
             cargo.rows.push(row)
         }
+        for (let i = 59; i < 68; i++) {
+            let row = {
+                Seq: rows[i][1],
+                Textual_reference: rows[i][2],
+                DG_Classification: rows[i][3],
+                IMO_hazard_classes: rows[i][4],
+                UN_number: rows[i][5],
+                Packing_group: rows[i][6],
+                Subsidiary_risk: rows[i][7],
+                Flash_point: rows[i][8],
+                pollution_code: rows[i][9],
+                EmS: rows[i][10],
+                Additional_information: rows[i][11],
+                Segregation_information: rows[i][12],
+                On_board_location: rows[i][13],
+            }
+            console.log(row)
+            dpg.rows.push(row)
+        }
+
 
         onSave({cargo});
 
