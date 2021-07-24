ec4066b2c803c2577b1660ed899db7a0e564d2a0
Mark Kravchuk <460888@student.saxion.nl>
2021-07-24 12:11:58 +0200

    Fixed spelling of cargoFormComponent.js

--- src/pages/shipDetails.js
+++ src/pages/shipDetails.js
@@ -32,7 +32,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import HealthFormComponent from "../components/blocks/healthFormComponent";
 import ShipStoresForm from "../components/blocks/shipStoresFormComponent";
 import CrewEffectsForm from "../components/blocks/crewEffectsFormComponent";
-import CargoForm from "../components/blocks/CargoFormComponent";
+import CargoForm from "../components/blocks/cargoFormComponent";
 import SecurityFormComponent from "../components/blocks/securityFormComponent";
 import DPGForm from "../components/blocks/dpgFormComponent";
 
